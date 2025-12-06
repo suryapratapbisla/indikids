ffe49876819561e51eda2d7512b5e1f34f8bb160
Surya Pratap Bisla <bislasuryapratap10@gmail.com>
2025-12-06 23:36:32 +0530

    Fixed the root dirct

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  return (
+  return (  
     <html lang="en" suppressHydrationWarning>
       <body>{children}</body>
     </html>
